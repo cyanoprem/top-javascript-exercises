c2581b6e883ef76e802416fc3b89a3516a70d44f
Cyano Prem <cyano.prem@gmail.com>
2022-07-14 18:40:54 +0530

    Complete 04 & 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+// const removeFromArray = function (array1, ...args) {
 
+//     args.forEach((arg) => {
+//       const index = array1.indexOf(arg);
+//       if (index > -1)
+//         array1.splice(index, 1);
+//     });
+//     return array1;
+      
+//   };
+
+function removeFromArray(array, ...args) {
+    return array.filter(x => !args.includes(x))
 };
 
 // Do not edit below this line
