2fc28da07243a3f88ad8e9b68dd7202b69af91b6
Cyano Prem <cyano.prem@gmail.com>
2022-07-14 10:58:30 +0530

    complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let Arr = text.split("");
+    Arr = Arr.reverse();
+    text = Arr.join("");
+    return text;
 };
 
 // Do not edit below this line
