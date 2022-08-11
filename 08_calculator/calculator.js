14476a29583ce4b5a504b20de52f5653b484def5
Cyano Prem <cyano.prem@gmail.com>
2022-08-11 16:09:28 +0530

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length == 0) {
+    return 0; 
+  } else {
+	  return (array.reduce((accumulator, currentValue) => accumulator + currentValue));
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return (array.reduce((accumulator, currentValue) => accumulator * currentValue));
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) {
+    return 1;
+  } else {
+    let fact = 1;
+    for (i=1; i<=a; i++) {
+      fact *=i;
+    }
+    return fact;
+  }
 };
 
 // Do not edit below this line
