9db08907a03f65e58fc8bd6086890035bb39a944
Cyano Prem <cyano.prem@gmail.com>
2022-08-11 16:40:52 +0530

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/[^a-zA-Z0-9]/g, '');
+    string = string.toLowerCase();
+    reverseString = string.split("").reverse().join("");
+    if (string === reverseString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
