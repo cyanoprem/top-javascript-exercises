5511b702767921f84b5c6da7c5e503147d0ab5c6
Cyano Prem <cyano.prem@gmail.com>
2022-08-11 21:44:00 +0530

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.forEach(assignYear);
+    
+    function assignYear (person) {
+        if (person.yearOfDeath == undefined) {
+        person['yearOfDeath'] = 2022;
+        }
+    }
+    
+    people.sort((a, b) => (b.yearOfDeath-b.yearOfBirth) - (a.yearOfDeath-a.yearOfBirth));
+    return people[0];
 };
 
 // Do not edit below this line
